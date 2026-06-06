79914f2b3c3b86a5ff315786c1e85cbae9cc6fae
ThatBaldDude4 <127453013+ThatBaldDude4@users.noreply.github.com>
2026-06-06 17:58:17 +0000

    create intial form data

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -1,6 +1,36 @@
 import { useState } from 'react'
 
+const intialFormData = {
+  contact: {
+    firstName: "",
+    lastName: "",
+    suffix: "",
+    email: "",
+    phone: "",
+    linkedin: "",
+    github: ""
+  },
+  education: [
+    {
+      id: crypto.randomUUID(),
+      school: "",
+      degree: "",
+    }
+  ],
+  experience: [
+    {
+      id: crypto.randomUUID(),
+      company: "",
+      startDate: "",
+      endDate: "",
+      skills: ""
+    }
+  ]
+};
+
 function App() {
+  const [formData, setFormData] = useState(intialFormData);
+
   return (
     <h1>MY APP</h1>
   )
