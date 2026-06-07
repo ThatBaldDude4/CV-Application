6a4868401721e54df213aa814e46b806e3e89130
ThatBaldDude4 <taylorv0004@gmail.com>
2026-06-07 16:43:51 -0500

    add helper function to update state arrays
    - add function for adding/deleteing object to
    arrays in state data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import Contact from './Contact';
+import Education from './Education';
 import './App.css';
+
+
 const intialFormData = {
   submitted: false,
   contact: {
@@ -43,10 +46,55 @@ function App() {
     }));
   };
 
+  function handleArrayField(arrayName, id, field, value) {
+    setFormData(prev => ({
+      ...prev,
+      [arrayName]: prev[arrayName].map((obj) => {
+        if (obj.id === id) {
+          return {...obj, [field]: value}
+        }else {
+          return obj;
+        }
+      })
+    }))
+  };
+
+  function addItemToArray(arrayName, newItem) {
+    setFormData(prev => ({
+      ...prev,
+      [arrayName]: [
+        ...prev[arrayName],
+        newItem
+      ]
+    }))
+  };
+
+  function removeItemFromArray(arrayName, itemId) {
+    setFormData(prev => ({
+      ...prev,
+      [arrayName]: prev[arrayName].filter((obj) => obj.id !== itemId)
+    }));
+  }
+
   return (
     <>
-      {!formData.submitted && <Contact handleContactField={handleContactField} contact={formData.contact} />}
+      {!formData.submitted &&
+        <> 
+        <Contact handleContactField={handleContactField} contact={formData.contact} />
+        <Education 
+          education={formData.education} 
+          handleArrayField={handleArrayField} 
+          addItemToArray={addItemToArray}
+          removeItemFromArray={removeItemFromArray}
+        />
+        </>
+      }
       <h2>Full Name: {formData.contact.firstName + " " + formData.contact.lastName + " " + formData.contact.suffix}</h2>
+
+      {/* this is testing the education form */}
+      {formData.education.map((obj) => {
+        return <div key={obj.id}>{obj.school}</div>
+      })}
     </>
   )
 }
