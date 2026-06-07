d218b05601361c6ea52d53d1d4a5d0dde916abc3
ThatBaldDude4 <taylorv0004@gmail.com>
2026-06-07 13:46:22 -0500

    make contact inputs controlled

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -4,18 +4,18 @@ export default function Contact({handleContactField, contact}) {
     const [showMoreContact, setShowMoreContact] = useState(false);
 
     return (
-        <section class="contact-section">
+        <section className="contact-section">
             <label>
                 First Name:
                 <input type="text" value={contact.firstName} onChange={(e) => {handleContactField("firstName", e.target.value)}}/>
             </label>
             <label>
                 Last Name:
-                <input type="text" onChange={(e) => {handleContactField("lastName", e.target.value)}}/>
+                <input type="text" value={contact.lastName} onChange={(e) => {handleContactField("lastName", e.target.value)}}/>
             </label>
             <label>
                 Suffix:
-                <select onChange={(e) => {handleContactField("suffix", e.target.value)}}>
+                <select value={contact.suffix} onChange={(e) => {handleContactField("suffix", e.target.value)}}>
                     <option value="">Select a Suffix</option>
                     <option value="Sr">Sr</option>
                     <option value="Jr">Jr</option>
@@ -26,23 +26,23 @@ export default function Contact({handleContactField, contact}) {
             </label>
             <label>
                 Email:
-                <input type="email" onChange={(e) => {handleContactField("email", e.target.value)}}/>
+                <input type="email" value={contact.email} onChange={(e) => {handleContactField("email", e.target.value)}}/>
             </label>
             <label>
                 Phone Number:
-                <input type="tel" onChange={(e) => {handleContactField("phone", e.target.value)}}/>
+                <input type="tel" value={contact.phone} onChange={(e) => {handleContactField("phone", e.target.value)}}/>
             </label>
-            <button onClick={() => {setShowMoreContact(prev => !prev)}}>+ Additional Contact +</button>
+            <button type="button" onClick={() => {setShowMoreContact(prev => !prev)}}>+ Additional Contact +</button>
 
             {showMoreContact && 
                 <>
                     <label>
                         Github:
-                        <input type="text" onchange={(e) => {handleContactField("github", e.target.value)}} />
+                        <input type="text" value={contact.github} onChange={(e) => {handleContactField("github", e.target.value)}} />
                     </label>
                     <label>
                         Linkedin:
-                        <input type="text" onchange={(e) => {handleContactField("linkedin", e.target.value)}} />
+                        <input type="text" value={contact.linkedin} onChange={(e) => {handleContactField("linkedin", e.target.value)}} />
                     </label>
                 </>
             }
