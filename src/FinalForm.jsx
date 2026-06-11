0c0078ce836b63cff8a4f5c2148340c1635332e4
ThatBaldDude4 <127453013+ThatBaldDude4@users.noreply.github.com>
2026-06-11 17:08:02 +0000

    add download pdf function

--- src/FinalForm.jsx
+++ src/FinalForm.jsx
@@ -1,75 +1,85 @@
+import { usePDF } from 'react-to-pdf';
 import './App.css';
+
 export default function FinalForm({formData}) {
+    const { toPDF, targetRef } = usePDF({filename: 'page.pdf'});
+
     return (
-        <div className="form-container">
-            <h1>
-                {(formData.contact.firstName && formData.contact.lastName) &&
-                `${formData.contact.firstName} ${formData.contact.lastName} ${formData.contact.suffix}`
-                }
-            </h1>
-            <div className="contact-form-container">
-                <h2>CONTACT</h2>
-                <div className="contact-form-data-container">
-                    <div className="email-container">
-                        {formData.contact.email}
-                    </div>
-
-                    <span className="divider-line"></span>
-
-                    <div className="phone-container">
-                        {formData.contact.phone}
-                    </div>
-
-                    {formData.contact.linkedin?.length > 0 &&
-                        <>
-                        <span className="divider-line"></span>
-                        <div className="linkedin-container">
-                            {formData.contact.linkedin}
-                        </div>
-                        </>
-                        
+        <>
+            <div ref={targetRef} className="form-container">
+                <h1>
+                    {(formData.contact.firstName && formData.contact.lastName) &&
+                    `${formData.contact.firstName} ${formData.contact.lastName} ${formData.contact.suffix}`
                     }
-                    {formData.contact.github?.length > 0 &&
-                        <>
-                        <span className="divider-line"></span>
-                        <div className="github-container">
-                            {formData.contact.github}
+                </h1>
+                <div className="contact-form-container">
+                    <h2>CONTACT</h2>
+                    <div className="contact-form-data-container">
+                        <div className="email-container">
+                            {formData.contact.email}
                         </div>
-                        </>
-                        
-                    }
-                </div>
-                
-            </div>
 
-            <div className="education-form-container">
-                <h2>EDUCATION</h2>
-                <div className = "education-form-data-container">
-                    {formData.education.map((data) => {
-                        return <div key={data.id}>
-                            <div>{data.school}</div>
-                            <div>{data.degree}</div>
-                            <div>{data.completionDate}</div>
+                        <span className="divider-line"></span>
+
+                        <div className="phone-container">
+                            {formData.contact.phone}
                         </div>
-                    })}
-                </div>
+
+                        {formData.contact.linkedin?.length > 0 &&
+                            <>
+                            <span className="divider-line"></span>
+                            <div className="linkedin-container">
+                                {formData.contact.linkedin}
+                            </div>
+                            </>
+                            
+                        }
+                        {formData.contact.github?.length > 0 &&
+                            <>
+                            <span className="divider-line"></span>
+                            <div className="github-container">
+                                {formData.contact.github}
+                            </div>
+                            </>
+                            
+                        }
+                    </div>
                     
-            </div>
+                </div>
 
-            <div className="experience-form-container">
-                <h2>EXPERIENCE</h2>
-                <div className="experience-form-data-container">
-                    {formData.experience.map((data) => {
-                    return <div key={data.id}>
-                        <div>{data.title}</div>
-                        <div>{data.company}</div>
-                        <div>{data.startDate}</div>
-                        <div>{data.endDate}</div>
-                        <div>{data.skills}</div>
+                <div className="education-form-container">
+                    <h2>EDUCATION</h2>
+                    <div className = "education-form-data-container">
+                        {formData.education.map((data) => {
+                            return <div key={data.id}>
+                                <div>{data.school}</div>
+                                <div>{data.degree}</div>
+                                <div>{data.completionDate}</div>
+                            </div>
+                        })}
                     </div>
-                    })}
+                        
+                </div>
+
+                <div className="experience-form-container">
+                    <h2>EXPERIENCE</h2>
+                    <div className="experience-form-data-container">
+                        {formData.experience.map((data) => {
+                        return <div key={data.id}>
+                            <div>{data.title}</div>
+                            <div>{data.company}</div>
+                            <div>{data.startDate}</div>
+                            <div>{data.endDate}</div>
+                            <div>{data.skills}</div>
+                        </div>
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
+            
+            <div className="download-button-container">
+                <button onClick={() => toPDF()}>Download PDF</button>
+            </div>
+        </>
     )
 };
